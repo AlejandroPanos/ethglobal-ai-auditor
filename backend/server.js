a48d8b8a955f9b9eca90b17957ca5c024d0ac3cb
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-25 13:49:53 +0200

    [add] - Fallback to port

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const auditRoutes = require("./routes/audit");
 
 /* Configure parsers */
