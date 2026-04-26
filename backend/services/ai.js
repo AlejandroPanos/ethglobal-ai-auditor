71a06f153459f48dece559387c388689e7484505
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-26 11:56:45 +0200

    [add] - Anthropic temp config

--- backend/services/ai.js
+++ backend/services/ai.js
@@ -1,5 +1,6 @@
 /* Imports */
 const Anthropic = require("@anthropic-ai/sdk");
+const { model } = require("mongoose");
 
 /* Configure Anthropic */
 const client = new Anthropic({
@@ -12,5 +13,10 @@ const analyzeContract = async (code) => {
     throw new Error("Code snippet is needed.");
   }
 
-  const message = await client.message.create({});
+  const message = await client.message.create({
+    model: "claude-haiku-4-5-20251001",
+    maxTokens: 1024,
+    system: "Placeholder",
+    messages: [{ role: "user", content: "Placeholder" }],
+  });
 };
