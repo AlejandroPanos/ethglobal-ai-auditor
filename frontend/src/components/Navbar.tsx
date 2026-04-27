1b73b77a8e0c9ca95c881d1594b3fc211fa66ef1
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-27 16:19:44 +0200

    [add] - Navbar component from shadcnui blocks

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,8 +1,30 @@
+import { Button } from "@/components/ui/button";
+import { Logo } from "@/components/logo";
+import { NavMenu } from "@/components/nav-menu";
+import { NavigationSheet } from "@/components/navigation-sheet";
+
 const Navbar = () => {
   return (
-    <>
-      <p>Navbar</p>
-    </>
+    <nav className="h-16 border-b bg-background">
+      <div className="mx-auto flex h-full max-w-(--breakpoint-xl) items-center justify-between px-4 sm:px-6 lg:px-8">
+        <Logo />
+
+        {/* Desktop Menu */}
+        <NavMenu className="hidden md:block" />
+
+        <div className="flex items-center gap-3">
+          <Button className="hidden sm:inline-flex" variant="outline">
+            Sign In
+          </Button>
+          <Button>Get Started</Button>
+
+          {/* Mobile Menu */}
+          <div className="md:hidden">
+            <NavigationSheet />
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 };
 
