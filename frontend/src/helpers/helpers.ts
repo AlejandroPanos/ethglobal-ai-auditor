5b0378fd0f80ac40dd54fd418234a7ed76aab044
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-28 10:39:56 +0200

    [add] - Helper functions

--- frontend/src/helpers/helpers.ts
+++ frontend/src/helpers/helpers.ts
@@ -2,3 +2,17 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
+
+export const generateAudit = async (code: string) => {
+  const response = await axios.post("/api/audit", { code });
+  const data = response.data;
+  return data;
+};
+
+export const retrieveAudit = async (rootHash: string) => {
+  const response = await axios.get(`/api/audit/${rootHash}`, {
+    responseType: "blob",
+  });
+  const data = response.data;
+  return data;
+};
