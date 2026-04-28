281ad0baa388d73cd55d1d328756ab2d22f1cbd0
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-28 21:20:36 +0200

    [add] - Input components

---
+++ frontend/src/components/customized/input/input-06.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+export default function InputWithButtonDemo() {
+  return (
+    <div className="flex w-full items-center gap-2">
+      <Input placeholder="0x123..." type="text" />
+      <Button className="shadow">Subscribe</Button>
+    </div>
+  );
+}
