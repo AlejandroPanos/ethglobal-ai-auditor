64ee5128ffbcb24ebb1bf96a69865949d64ff341
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-28 09:40:15 +0200

    [add] - Code editor header

--- frontend/src/features/code/components/CodeEditor.tsx
+++ frontend/src/features/code/components/CodeEditor.tsx
@@ -1,16 +1,20 @@
 import Banner from "@/components/Banner";
+import { Button } from "@/components/ui/button";
 import { FileBracesCorner } from "lucide-react";
 
 const CodeEditor = () => {
   return (
     <>
       <div className="flex items-center justify-center my-8 px-6 ">
-        <div className="max-w-4xl">
-          <Banner
-            icon={FileBracesCorner}
-            title="Smart Contract Code"
-            description="Paste your Solidity code below."
-          />
+        <div className="w-full max-w-3xl">
+          <div className="flex items-center justify-between w-full gap-4">
+            <Banner
+              icon={FileBracesCorner}
+              title="Smart Contract Code"
+              description="Paste your Solidity code below."
+            />
+            <Button>Load Example</Button>
+          </div>
         </div>
       </div>
     </>
