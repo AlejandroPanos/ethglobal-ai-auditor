6a722ea66691ef5df8a392b9c36fb6eeb18a9b58
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-28 09:54:53 +0200

    [add] - Code editor

--- frontend/src/features/code/components/CodeEditor.tsx
+++ frontend/src/features/code/components/CodeEditor.tsx
@@ -1,11 +1,12 @@
 import Banner from "@/components/Banner";
 import { Button } from "@/components/ui/button";
-import { FileBracesCorner } from "lucide-react";
+import Editor from "@monaco-editor/react";
+import { FileBracesCorner, Sparkles } from "lucide-react";
 
 const CodeEditor = () => {
   return (
     <>
-      <div className="flex items-center justify-center my-8 px-6 ">
+      <div className="flex items-center justify-center my-12 px-6 ">
         <div className="w-full max-w-3xl">
           <div className="flex items-center justify-between w-full gap-4">
             <Banner
@@ -14,6 +15,26 @@ const CodeEditor = () => {
               description="Paste your Solidity code below."
             />
             <Button>Load Example</Button>
+          </div>
+
+          <div className="w-full flex flex-col items-center justify-center gap-6">
+            <div className="w-full rounded-xl overflow-hidden border border-white/20 mt-4">
+              <Editor
+                height="80vh"
+                language="sol"
+                theme="vs-dark"
+                defaultValue="// Paste your Solidity code here..."
+                options={{
+                  fontSize: 12,
+                  minimap: { enabled: false },
+                  scrollBeyondLastLine: false,
+                  padding: { top: 16 },
+                }}
+              />
+            </div>
+            <Button size="lg">
+              <Sparkles /> Start Audit
+            </Button>
           </div>
         </div>
       </div>
