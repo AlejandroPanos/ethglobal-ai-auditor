ffb874a1acefd31b45f3608dff01b86a1b2021c2
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-28 10:21:18 +0200

    [add] - Solidity example loading

--- frontend/src/features/code/components/CodeEditor.tsx
+++ frontend/src/features/code/components/CodeEditor.tsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import Banner from "@/components/Banner";
 import { Button } from "@/components/ui/button";
 import Editor from "@monaco-editor/react";
 import { FileBracesCorner, Sparkles } from "lucide-react";
+import { EXAMPLE_CONTRACT } from "@/constants/exampleContract";
 
 const CodeEditor = () => {
+  const [code, setCode] = useState("");
+
+  const handleLoadExample = () => {
+    setCode(EXAMPLE_CONTRACT);
+  };
+
   return (
     <>
       <div className="flex items-center justify-center my-12 px-6 ">
@@ -14,16 +22,18 @@ const CodeEditor = () => {
               title="Smart Contract Code"
               description="Paste your Solidity code below."
             />
-            <Button>Load Example</Button>
+            <Button onClick={() => handleLoadExample()}>Load Example</Button>
           </div>
 
           <div className="w-full flex flex-col items-center justify-center gap-6">
             <div className="w-full rounded-xl overflow-hidden border border-white/20 mt-4">
               <Editor
-                height="80vh"
+                height="60vh"
                 language="sol"
                 theme="vs-dark"
                 defaultValue="// Paste your Solidity code here..."
+                value={code}
+                onChange={(value) => setCode(value || "")}
                 options={{
                   fontSize: 12,
                   minimap: { enabled: false },
