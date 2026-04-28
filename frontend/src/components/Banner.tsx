7c6683a7833ad22e1cb51894d67609d957d2703d
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-28 09:36:13 +0200

    [update] - Banner style & responsiveness

--- frontend/src/components/Banner.tsx
+++ frontend/src/components/Banner.tsx
@@ -9,12 +9,14 @@ interface BannerProps {
 const Banner = ({ icon: Icon, title, description }: BannerProps) => {
   return (
     <>
-      <div className="flex flex-col rounded-xl border px-5 py-6">
-        <div className="mb-4 flex h-10 w-10 items-center justify-center rounded-xl bg-green-100/10">
+      <div className="flex flex-col items-center text-center gap-3 sm:flex-row sm:items-center sm:text-left sm:gap-4">
+        <div className="flex shrink-0 h-10 w-10 items-center justify-center rounded-xl bg-green-100/10">
           <Icon className="size-6 text-green-600" />
         </div>
-        <span className="font-medium text-lg">{title}</span>
-        <p className="mt-1 text-[15px] text-foreground/60">{description}</p>
+        <div className="flex flex-col items-center sm:items-start justify-center">
+          <span className="font-medium text-lg">{title}</span>
+          <p className="text-sm font-light text-foreground/60">{description}</p>
+        </div>
       </div>
     </>
   );
