c4c7977c3f6d461a0acbe4afb5d7c108d2e56d4a
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-29 09:13:38 +0200

    [remove] - Strict mode for testing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
@@ -8,8 +8,6 @@ const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
-    <StrictMode>
-      <App />
-    </StrictMode>
+    <App />
   </QueryClientProvider>,
 );
